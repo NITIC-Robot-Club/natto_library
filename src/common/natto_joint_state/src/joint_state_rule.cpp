cc9387e5be7fb2fc6344ec9b4dcf2f37a7ec9421
RIKKUN525 <yuka314413@gmail.com>
2026-08-29 17:32:18 +0900

    joint-state-rule-velocity-stop

--- src/common/natto_joint_state/src/joint_state_rule.cpp
+++ src/common/natto_joint_state/src/joint_state_rule.cpp
@@ -144,8 +144,16 @@ void joint_state_rule::command_joint_states_callback (const sensor_msgs::msg::Jo
             auto it_fixed = std::find (fixed_command.name.begin (), fixed_command.name.end (), rule.then_condition.joint_name);
             if (it_fixed != fixed_command.name.end ()) {
                 size_t index                  = static_cast<size_t> (std::distance (fixed_command.name.begin (), it_fixed));
-                double value                  = fixed_command.position[index];
-                fixed_command.position[index] = std::clamp (value, rule.then_condition.min, rule.then_condition.max);
+                if (index < fixed_command.position.size ()) {
+                    double value                  = fixed_command.position[index];
+                    fixed_command.position[index] = std::clamp (value, rule.then_condition.min, rule.then_condition.max);
+                }
+
+                // Stop the target joint when a rule is activated, including velocity-controlled joints.
+                if (fixed_command.velocity.size () < fixed_command.name.size ()) {
+                    fixed_command.velocity.resize (fixed_command.name.size (), 0.0);
+                }
+                fixed_command.velocity[index] = 0.0;
             }
         }
     }
